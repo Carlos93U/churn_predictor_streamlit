d697f614e3cea69c4de317f539e6c83c8e6acb1d
carlos <huillcas.juan3@gmail.com>
2024-03-24 20:01:00 -0500

    First Version streamlit

--- README.md
+++ README.md
@@ -1,2 +1,105 @@
-# churn_predictor_streamlit
-Deployment of an ML model with Streamlit to predict churn in telecommunications, optimizing customer retention.
+# CHURN_PREDICTOR | <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/800px-Python-logo-notext.svg.png" height=20> | <img src="https://i.postimg.cc/cHqB5VtL/scikit-learn-logo.png" height=20>| <img src="https://pandas.pydata.org/static/img/pandas_white.svg" height=20>  | <img src="https://i.postimg.cc/m2dwfTdm/numpy-logo.png" height=20> |  <img src="https://matplotlib.org/_static/logo_dark.svg" height=20> | <img src="https://seaborn.pydata.org/_static/logo-wide-lightbg.svg" height=20> | <img src="https://assets.ydata.ai/oss/ydata-profiling_red.png" height=20> |[<img src="https://static-00.iconduck.com/assets.00/github-icon-2048x1988-jzvzcf2t.png" height=20> ](https://github.com/Carlos93U) | [<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png" height=20>](https://www.linkedin.com/in/juan-carlos-huillcas/) |
+
+## 1. Resume
+
+The project focuses on a dataset from a telecommunications company to predict if customers will churn. It utilizes the scikit-learn library, specifically the binomial logistic regression model. The analysis involves exploring relevant variables and implementing the model to predict churn. The aim is to understand the factors influencing customer churn and provide a strategy to retain them using supervised learning techniques.
+
+
+<center>
+
+[![yprofile-online-video-cutter-com.gif](https://i.postimg.cc/L5632mwF/yprofile-online-video-cutter-com.gif)](https://postimg.cc/Pp9wMndS)
+
+
+</center>
+
+
+## 2. Data Set Characteristics
+
+The churn_data contains the following features:
+
+
+| Field              | Description                                                                                                             |
+|--------------------|-------------------------------------------------------------------------------------------------------------------------|
+| `customerID`       | A unique ID that identifies each customer                                                                                 |
+| `Gender`           | The customer's gender: Male, Female                                                                                       |
+| `SeniorCitizen`    | Indicates if the customer is 65 or older: Yes, No                                                                        |
+| `Partner`          | Indicates if the customer is married: Yes, No                                                                             |
+| `Dependents`       | Indicates if the customer lives with any dependents: Yes, No. Dependents could be children, parents, grandparents, etc.  |
+| `Tenure`           | Indicates the total amount of months that the customer has been with the company.                                          |
+| `PhoneService`     | Indicates if the customer subscribes to home phone service with the company: Yes, No                                       |
+| `MultipleLines`    | Indicates if the customer subscribes to multiple telephone lines with the company: Yes, No                                |
+| `InternetService`  | Indicates if the customer subscribes to Internet service with the company: No, DSL, Fiber Optic, Cable.                   |
+| `OnlineSecurity`   | Indicates if the customer subscribes to an additional online security service provided by the company: Yes, No            |
+| `OnlineBackup`     | Indicates if the customer subscribes to an additional online backup service provided by the company: Yes, No             |
+| `DeviceProtection` | Indicates if the customer subscribes to an additional device protection plan for their Internet equipment provided by the company: Yes, No |
+| `TechSupport`      | Indicates if the customer subscribes to an additional technical support plan from the company with reduced wait times: Yes, No |
+| `StreamingTV`      | Indicates if the customer uses their Internet service to stream television programming from a third-party provider: Yes, No. The company does not charge an additional fee for this service. |
+| `StreamingMovies`  | Indicates if the customer uses their Internet service to stream movies from a third-party provider: Yes, No. The company does not charge an additional fee for this service. |
+| `Contract`         | Indicates the customer's current contract type: Month-to-Month, One Year, Two Year                                         |
+| `PaperlessBilling` | Indicates if the customer has chosen paperless billing: Yes, No                                                           |
+| `PaymentMethod`    | Indicates how the customer pays their bill: Bank Withdrawal, Credit Card, Mailed Check                                     |
+| `MonthlyCharge`    | Indicates the customer's current total monthly charge for all their services from the company.                              |
+| `TotalCharges`     | Indicates the customer's total charges, calculated to the end of the quarter.                                              |
+| `Churn`            | Yes = the customer left the company this quarter. No = the customer remained with the company                                |
+
+
+## 3. Setting up
+
+*Create a virtual enviroment with:*
+
+```
+python3 -m venv env
+
+```
+*Activate virtual enviroment:*
+
+```
+source env/bin/activate
+```
+
+*Install requirements*
+
+```
+pip install -r requirements.txt
+```
+
+## 4. Running
+
+* *Open a churn predictor notebook*
+* *Run All*
+* *See outputs*
+
+Correlation values about target variable:
+
+[![out1.png](https://i.postimg.cc/4NNYZsbw/out1.png)](https://postimg.cc/9zKmY3kq)
+
+Churn vs Gender
+
+[![out2.png](https://i.postimg.cc/WzshdvRN/out2.png)](https://postimg.cc/CB6F29LX)
+
+Top 10 variables that contrinute to churn
+
+[![out3.png](https://i.postimg.cc/L5vhfbRz/out3.png)](https://postimg.cc/fStwQ5VL)
+
+## 5. Conclutions:
+
+* Approximately the same amount of men as women churn.
+* Month-to-month contracts factor heavily into a user's decision to churn.
+* Lack of online security and technical support greatly contribute to churn.
+* High monthly charges and short tenure influence users to churn.
+* Having a two-year contract is a good indicator that the user will not churn from the company.
+* Variables contributing the most precision to the model can be selected progressively.
+
+
+## 5. Libraries and documentation
+
+* [Python](https://www.python.org/doc/)
+* [sklearn](https://scikit-learn.org/stable/)
+* [pandas](https://pandas.pydata.org/)
+* [numpy](https://numpy.org/)
+* [matplotlib](https://matplotlib.org/)
+* [seaborn](https://seaborn.pydata.org/index.html#)
+* [ydata_profiling](https://docs.profiling.ydata.ai/latest/)
+
+## 6. Sources
+* [Kaggle](https://www.kaggle.com/datasets/blastchar/telco-customer-churn)
---
+++ deploy/deploy_streamlit.py
@@ -0,0 +1,114 @@
+import pickle
+
+import pandas as pd
+import streamlit as st
+
+with open("../models/churn_model.pkl", "rb") as f:
+    trained_model = pickle.load(f)
+
+st.set_page_config(page_title="Churn Predictor")
+
+st.title("Churn Predictor Model")
+
+st.write(
+    "Enter values about features to get a prediction."
+)
+
+# Get input data from user
+SeniorCitizen = st.number_input("Senior Citizen:", value=0, min_value=0, max_value=1)
+tenure = st.number_input("Tenure:", value=0, min_value=0, max_value=100)
+MonthlyCharges = st.number_input("Monthly Charges:", value=0.0, min_value=0.0, max_value=9999.99)
+TotalCharges = st.number_input("Total Charges:", value=0.0, min_value=0.0, max_value=99999.99)
+Partner_No = st.number_input("Partner (No):", value=0, min_value=0, max_value=1)
+Partner_Yes = st.number_input("Partner (Yes):", value=0, min_value=0, max_value=1)
+Dependents_No = st.number_input("Dependents (No):", value=0, min_value=0, max_value=1)
+Dependents_Yes = st.number_input("Dependents (Yes):", value=0, min_value=0, max_value=1)
+MultipleLines_No = st.number_input("Multiple Lines (No):", value=0, min_value=0, max_value=1)
+MultipleLines_No_phone_service = st.number_input("Multiple Lines (No phone service):", value=0, min_value=0, max_value=1)
+MultipleLines_Yes = st.number_input("Multiple Lines (Yes):", value=0, min_value=0, max_value=1)
+InternetService_DSL = st.number_input("Internet Service (DSL):", value=0, min_value=0, max_value=1)
+InternetService_Fiber_optic = st.number_input("Internet Service (Fiber optic):", value=0, min_value=0, max_value=1)
+InternetService_No = st.number_input("Internet Service (No):", value=0, min_value=0, max_value=1)
+OnlineSecurity_No = st.number_input("Online Security (No):", value=0, min_value=0, max_value=1)
+OnlineSecurity_No_internet_service = st.number_input("Online Security (No internet service):", value=0, min_value=0, max_value=1)
+OnlineSecurity_Yes = st.number_input("Online Security (Yes):", value=0, min_value=0, max_value=1)
+OnlineBackup_No = st.number_input("Online Backup (No):", value=0, min_value=0, max_value=1)
+OnlineBackup_No_internet_service = st.number_input("Online Backup (No internet service):", value=0, min_value=0, max_value=1)
+OnlineBackup_Yes = st.number_input("Online Backup (Yes):", value=0, min_value=0, max_value=1)
+DeviceProtection_No = st.number_input("Device Protection (No):", value=0, min_value=0, max_value=1)
+DeviceProtection_No_internet_service = st.number_input("Device Protection (No internet service):", value=0, min_value=0, max_value=1)
+DeviceProtection_Yes = st.number_input("Device Protection (Yes):", value=0, min_value=0, max_value=1)
+TechSupport_No = st.number_input("Tech Support (No):", value=0, min_value=0, max_value=1)
+TechSupport_No_internet_service = st.number_input("Tech Support (No internet service):", value=0, min_value=0, max_value=1)
+TechSupport_Yes = st.number_input("Tech Support (Yes):", value=0, min_value=0, max_value=1)
+StreamingTV_No = st.number_input("Streaming TV (No):", value=0, min_value=0, max_value=1)
+StreamingTV_No_internet_service = st.number_input("Streaming TV (No internet service):", value=0, min_value=0, max_value=1)
+StreamingTV_Yes = st.number_input("Streaming TV (Yes):", value=0, min_value=0, max_value=1)
+StreamingMovies_No = st.number_input("Streaming Movies (No):", value=0, min_value=0, max_value=1)
+StreamingMovies_No_internet_service = st.number_input("Streaming Movies (No internet service):", value=0, min_value=0, max_value=1)
+StreamingMovies_Yes = st.number_input("Streaming Movies (Yes):", value=0, min_value=0, max_value=1)
+Contract_Month_to_month = st.number_input("Contract (Month-to-month):", value=0, min_value=0, max_value=1)
+Contract_One_year = st.number_input("Contract (One year):", value=0, min_value=0, max_value=1)
+Contract_Two_year = st.number_input("Contract (Two year):", value=0, min_value=0, max_value=1)
+PaperlessBilling_No = st.number_input("Paperless Billing (No):", value=0, min_value=0, max_value=1)
+PaperlessBilling_Yes = st.number_input("Paperless Billing (Yes):", value=0, min_value=0, max_value=1)
+PaymentMethod_Bank_transfer_automatic = st.number_input("Payment Method (Bank transfer automatic):", value=0, min_value=0, max_value=1)
+PaymentMethod_Credit_card_automatic = st.number_input("Payment Method (Credit card automatic):", value=0, min_value=0, max_value=1)
+PaymentMethod_Electronic_check = st.number_input("Payment Method (Electronic check):", value=0, min_value=0, max_value=1)
+PaymentMethod_Mailed_check = st.number_input("Payment Method (Mailed check):", value=0, min_value=0, max_value=1)
+
+
+input_data = pd.DataFrame(
+    [
+        {
+            "SeniorCitizen": SeniorCitizen,
+            "tenure": tenure,
+            "MonthlyCharges": MonthlyCharges,
+            "TotalCharges": TotalCharges,
+            "Partner_No": Partner_No,
+            "Partner_Yes": Partner_Yes,
+            "Dependents_No": Dependents_No,
+            "Dependents_Yes": Dependents_Yes,
+            "MultipleLines_No": MultipleLines_No,
+            "MultipleLines_No phone service": MultipleLines_No_phone_service,
+            "MultipleLines_Yes": MultipleLines_Yes,
+            "InternetService_DSL": InternetService_DSL,
+            "InternetService_Fiber optic": InternetService_Fiber_optic,
+            "InternetService_No": InternetService_No,
+            "OnlineSecurity_No": OnlineSecurity_No,
+            "OnlineSecurity_No internet service": OnlineSecurity_No_internet_service,
+            "OnlineSecurity_Yes": OnlineSecurity_Yes,
+            "OnlineBackup_No": OnlineBackup_No,
+            "OnlineBackup_No internet service": OnlineBackup_No_internet_service,
+            "OnlineBackup_Yes": OnlineBackup_Yes,
+            "DeviceProtection_No": DeviceProtection_No,
+            "DeviceProtection_No internet service": DeviceProtection_No_internet_service,
+            "DeviceProtection_Yes": DeviceProtection_Yes,
+            "TechSupport_No": TechSupport_No,
+            "TechSupport_No internet service": TechSupport_No_internet_service,
+            "TechSupport_Yes": TechSupport_Yes,
+            "StreamingTV_No": StreamingTV_No,
+            "StreamingTV_No internet service": StreamingTV_No_internet_service,
+            "StreamingTV_Yes": StreamingTV_Yes,
+            "StreamingMovies_No": StreamingMovies_No,
+            "StreamingMovies_No internet service": StreamingMovies_No_internet_service,
+            "StreamingMovies_Yes": StreamingMovies_Yes,
+            "Contract_Month-to-month": Contract_Month_to_month,
+            "Contract_One year": Contract_One_year,
+            "Contract_Two year": Contract_Two_year,
+            "PaperlessBilling_No": PaperlessBilling_No,
+            "PaperlessBilling_Yes": PaperlessBilling_Yes,
+            "PaymentMethod_Bank transfer (automatic)": PaymentMethod_Bank_transfer_automatic,
+            "PaymentMethod_Credit card (automatic)": PaymentMethod_Credit_card_automatic,
+            "PaymentMethod_Electronic check": PaymentMethod_Electronic_check,
+            "PaymentMethod_Mailed check": PaymentMethod_Mailed_check
+        }
+    ]
+)
+
+
+# Make prediction
+prediction = trained_model.predict(input_data)
+
+# Display prediction
+st.write("Prediction:", prediction[0])
